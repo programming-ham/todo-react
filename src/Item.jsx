1ec22364bf58f0a4ea8a05abcd53fc649b74cb15
Hayato Sato <programming.ham@gmail.com>
2021-03-17 23:39:02 +0900

    少しのスタイル追加

--- src/Item.jsx
+++ src/Item.jsx
@@ -4,15 +4,16 @@ import {FaCheckCircle, FaTrashAlt, FaPen} from "react-icons/fa";
 const Item = ({content, id, deleteTodo}) => {
   const [done, setDone] = useState(false);
   return (
-    <ul>
+    <ul className = "itemInner">
       <li>
-        <span style = {{textDecoration: done ? "line-through": "none"}}>{content}</span>
-        <button><FaPen /></button>
-        <button onClick = {() => setDone(!done)}><FaCheckCircle /></button>
-        <button onClick = {() => deleteTodo(id)}><FaTrashAlt /></button>
-        
-      
+        <span className = "task" style = {{textDecoration: done ? "line-through": "none"}}>{content}</span>
       </li>
+      <div className = "btns">
+        <button className = " btn edit"><FaPen /></button>
+        <button className = "btn done" onClick = {() => setDone(!done)}><FaCheckCircle /></button>
+        <button className = "btn delete" onClick = {() => deleteTodo(id)}><FaTrashAlt /></button>
+      </div>
+      
     </ul>
   );
 };
