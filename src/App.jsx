844a0b643077b4037e181ea2fbdbd77dfda4ff73
Hayato Sato <programming.ham@gmail.com>
2021-06-10 13:58:09 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,6 @@ const App = () => {
       .then(querySnapshot => {
         const data = querySnapshot.docs.map(doc => ({...doc.data(), id: doc.id,
         }));
-        console.log(data);
         setTasks(data);
       })
       .catch(err => {
